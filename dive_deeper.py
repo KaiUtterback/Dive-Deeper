01333cf0c4d22570f1bb758328fbfbabb2787d94
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-08 11:21:28 -0600

    add questions 1 and 2

--- dive_deeper.py
+++ dive_deeper.py
@@ -1,1 +1,26 @@
-# does this work
+# 1 Decisions at the Crossroad
+# Question 1 Task 1 Code Correction
+
+number = int(input("Enter a number"))
+
+if number > 0:
+    print("The Number is positive.")
+elif number == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative")
+
+# Question 2 Task 1 Code Correction
+    
+choice = input("Do you choose the path to the left or right? ")
+
+if choice == "left":
+    print("You find a treasure chest!")
+elif choice == "right":
+    print("You encounter a dragon!")
+else: 
+    print("Invalid choice!")
+
+# Question 3
+    
+# Task 1
