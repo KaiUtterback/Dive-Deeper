bf69a20b5b4623a16355af815a6d99b815c1da04
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-08 11:55:40 -0600

    work

--- dive_deeper.py
+++ dive_deeper.py
@@ -1,7 +1,9 @@
 # 1 Decisions at the Crossroad
 # Question 1 Task 1 Code Correction
+print( )
+print("-" * 50)
 
-number = int(input("Enter a number"))
+number = int(input("Enter a number: "))
 
 if number > 0:
     print("The Number is positive.")
@@ -11,6 +13,7 @@ else:
     print("The number is negative")
 
 # Question 2 Task 1 Code Correction
+print("-" * 50)
     
 choice = input("Do you choose the path to the left or right? ")
 
@@ -22,5 +25,39 @@ else:
     print("Invalid choice!")
 
 # Question 3
+print("-" * 50)
     
-# Task 1
+""" 
+Task 1 Identify the Greatest:L
+Write a Python program that prompts the user to enter three numbers.
+The program should then identify and print out the largest number of the three.
+"""
+
+print("Please enter 3 numbers below.")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+largest = 0
+
+if num1 >= largest:
+    largest = num1
+if num2 >= largest:
+    largest = num2
+if num3 >= largest:
+    largest = num3
+
+print("The largest number is ", largest)
+
+# Task 2
+
+smallest = 0
+
+if num1 <= largest:
+    smallest = num1
+if num2 <= smallest:
+    smallest = num2
+if num3 <= smallest:
+    smallest = num3 
+    
+print("The smallest is", smallest)
